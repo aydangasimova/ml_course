e916d06257e899860f84c77c2965dbb140fcb805
aydan <a.gsmva@gmail.com>
2019-03-11 23:53:19 +0100

    started solving ass1

--- pandas_demo.py
+++ pandas_demo.py
@@ -1,6 +1,19 @@
 import numpy as np
-
 import pandas as pd
+# we don't like warnings
+# you can comment the following 2 lines if you'd like to
+import warnings
+warnings.filterwarnings('ignore')
 
+#df = pd.read_csv('/home/aydan/Desktop/Courses/ml_course/data/telecom_churn.csv')
+df = pd.read_csv('data/telecom_churn.csv')
+print(df.head())
 
-print("hello")
+print(df.shape)
+print(df.columns)
+print(df.info())
+
+df['Churn'] = df['Churn'].astype('int64')
+df.describe()
+
+df.describe(include=['object', 'bool'])
